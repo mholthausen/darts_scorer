a5748f087be1edd428c4aa64b3ebaf7ede88a305
Michael Holthausen <mholthausen@terrestris.de>
2021-02-24 21:47:08 +0100

    Fix linting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import "./styles/styles.css";
+import './styles/styles.css';
 
 /**
  * The Application
  */
 const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>DartsScorer - Results</h1>
       <h2>…</h2>
     </div>
